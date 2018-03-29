45fda6d2aa702f33b6cdd68583d97ec595127226
Evgeniy Derz <eug_13@step.itstep.rv.ua>
2018-03-29 21:14:03 +0300

    Classes

--- index.js
+++ index.js
@@ -409,3 +409,173 @@
 //           </div> `;
 
 // document.write(tag);                  
+
+
+// 20.03.18 Class work
+
+// let arr =[1,2,3,4,5,6,7];
+
+// for (let item of arr){
+//     console.log(item);
+// }
+
+
+
+
+//OOD IN JAVASCRIPT CLASSES AND OBJECT OF CLASS
+
+// CLASS DOG
+
+// class Dog {
+//     constructor(name,type,age,energy){
+//         this._name = name;
+//         this._type = type;
+//         this._age = age;
+//         this._energy = energy;
+//         this.getInfo();
+//         // this.getPlay();
+//         // this.getEat();
+//         // this.getSleep();
+
+//     }
+
+//     getInfo(){
+//         console.log(`${this._name}  ${this._type} ${this._age} ${this._energy}`);
+//     }
+
+//     getPlay(){
+//          this._energy -=30;
+//          this.getInfo();
+//          this.getDie();
+//     }
+//     getEat(){
+//         this._energy +=20;
+//         this.getInfo();
+//     }
+//     getSleep(){
+//         this._energy +=30;
+//         this.getInfo();
+//     }
+//     getDie(){
+//         if(this._energy<=0){
+//             alert("Your dog "+this._name+" is dead!");
+//         }
+
+//     }
+// }
+
+// let Bobik = new Dog("Bobik","Dvorterer",18,100);
+// let Gav = new Dog("GAV","Boxer",5,100);
+
+// console.log(Bobik);
+// console.log(Gav);
+// Bobik.getInfo();
+// Bobik.getPlay();
+
+// // Changing name inside obj
+
+// Bobik._name="Tuzik";
+// console.log(Bobik._name);//in console will see Tuzik
+
+// GETTER AND SETTER
+
+// class Dog {
+//     constructor(name, type, age, energy) {
+//         this._name = name;
+//         this._type = type;
+//         this._age = age;
+//         this._energy = energy;
+//         this.getInfo();
+//     }
+
+//     getInfo() {
+//         console.log(`${this._name}  ${this._type} ${this._age} ${this._energy}`);
+//     }
+
+//     get name() {
+//         return this._name;
+//     }
+//     //Here I catch parameters from Bobik1
+//     set name(name){
+//         if(name != this._name){
+//             this._name = name;
+//         }
+//     }
+
+
+// }
+
+
+// let Bobik = new Dog("Bobik", "Dvorterer", 18, 100);
+
+
+// console.log(Bobik.name);
+
+// Bobik.name = "Bobik1";//Here Bobik name changes on Bobik1
+
+// console.log(Bobik.name);
+
+// Bobik._name = "Bobik2"; // Here without getter and setter
+// console.log(Bobik.name);
+
+
+// DIFERENCES BETWEEN OBJECT DOG 
+//Nasliduvannja
+
+class Dog {
+    constructor(name, type, age, energy) {
+        this._name = name;
+        this._type = type;
+        this._age = age;
+        this._energy = energy;
+        // this._counter = 0;
+        this.getInfo();
+        // Dog.counter();
+    }
+
+    getInfo() {
+        console.log(`${this._name}  ${this._type} ${this._age} ${this._energy}`);
+    }
+
+    // static counter() {
+    //  this._counter ++;
+    //      console.log(`Dog constructor ${this._counter}`);
+    // }
+
+}
+
+
+let Bobik = new Dog("Bobik", "Dvorterer", 18, 100);
+
+// Bobik.getInfo();
+// console.log(Bobik.name);
+
+
+class HunterDog extends Dog {
+    constructor(name, type, age, energy, power) {
+        super(name, type, age, energy);//Parent constructor parameters
+        this._power = power;
+        console.log("HUNTER DOG");
+
+    }
+    getInfo() {
+        console.log(`${this._name}  ${this._type} ${this._age} ${this._energy} ${this._power}`);
+    }
+
+    canHunt() {
+        // let timer = setInterval()
+        this._energy -= 50;
+
+    }
+}
+
+let Killer = new HunterDog("killer", "boxer", 1, 100, "crazy power");
+
+Killer.getInfo();
+Killer.canHunt();
+
+Killer.getInfo();
+
+console.log(Killer._energy);
+
+// Dog.counter();
